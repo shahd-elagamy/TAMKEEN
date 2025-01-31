4325aaf41e58739c3e8c675beb68e26151511dba
Shahd Elagamy <104149324+Aki52003@users.noreply.github.com>
2025-01-31 22:17:56 +0200

    Update urls.py

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import submit_answers
+from .views import submit_answers, QuestionViewSet, AnswerViewSet
 
 router = DefaultRouter()
-# Register viewsets, not serializers
-# router.register(r'questions', QuestionViewSet)  # Use the ViewSet for questions
-# router.register(r'answers', AnswerViewSet)      # Use the ViewSet for answers
+router.register(r'questions', QuestionViewSet, basename='question')  # استخدم ViewSet بدلاً من Serializer
+router.register(r'answers', AnswerViewSet, basename='answer')        # استخدم ViewSet بدلاً من Serializer
 
 urlpatterns = [
     path('signup/questionnaire/questionnaire_view', views.questionnaire_view, name='questionnaire_view'),
-    # path('questions/', views.quiz_questions_view, name='quiz_questions'),
     path('quiz_view', views.quiz_view, name='quiz_view'),
     path('quiz_form/questionnaire_completed/', views.questionnaire_completed, name='questionnaire_completed'),
-    path('api/', include(router.urls)),  # Include the API routes for questions and answers
-    # path('questions/', views.show_questions, name='show_questions'),
-    path('api/submit-answers/', submit_answers,name='submit_answers'),
+    path('api/', include(router.urls)),  # تضمين API المسارات الصحيحة
+    path('api/submit-answers/', submit_answers, name='submit_answers'),
 ]
